263bc0d3c6589a60c17d69f153d7f5c3a253adb4
Guido Fonticelli <azuresky07@gmail.com>
2016-06-06 20:50:44 -0300

    corecciones menores

--- app/Http/routes/admin.routes.php
+++ app/Http/routes/admin.routes.php
@@ -4,8 +4,8 @@ Route::get('/',function(){
     return view('dashboard.index');
 });
 
-Route::resource('cosplay', 'cosplayController');
-Route::get('/cosplay/{cosplay}/{tab}', array('as' => 'admin.cosplay.showtab', 'uses' => 'cosplayController@showtab') );
+Route::resource('cosplay', 'CosplayController');
+Route::get('/cosplay/{cosplay}/{tab}', array('as' => 'admin.cosplay.showtab', 'uses' => 'CosplayController@showtab') );
 Route::resource('cosplay.parts','CosplayPartController');
 
 Route::resource('cosplay.gastos','CosplayGastosController');
